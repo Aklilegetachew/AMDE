6359c33cad65dbce25d9017a42601b1d7c119c6c
Aklilegetachew <aklilegetachew@gmail.com>
2024-08-12 19:08:27 +0300

    haum

--- src/app/Component/AnimatedMenu.tsx
+++ src/app/Component/AnimatedMenu.tsx
@@ -3,6 +3,8 @@
 import React, { useRef, useState } from "react";
 import gsap from "gsap";
 import HamburgerMenu from "../../../public/hamburger-menu.svg";
+import BackgroundOverlay from "../../../public/backgroudoverlay.svg";
+import Socials from "../../../public/socials.svg";
 import Logo from "../../../public/logo-gray.svg";
 import X from "../../../public/x.svg";
 
@@ -51,21 +53,56 @@ const AnimatedMenu: React.FC = () => {
       </button>
       <div
         ref={menuRef}
-        className={`fixed top-0 left-0 w-full h-full secondary-color  text-white flex transform transition-transform duration-500 ease-in-out ${
+        className={`fixed top-0 left-0 w-full h-full secondary-color   text-white flex item-center  transform transition-transform duration-500 ease-in-out ${
           isOpen ? "translate-x-0 opacity-100" : "-translate-x-full opacity-0"
         }`}
       >
-        <nav className=" fixed w-full top-10 left-0 h-10 p-10 flex justify-between items-center">
+        <nav className=" fixed w-full top-8 left-0 h-10 p-10  flex justify-between items-center">
           <button onClick={toggleMenu} className="flex items-center">
-            <X />
+            <X className="w-auto h-auto" />
           </button>
 
           <div className="flex items-center">
-            <Logo className="w-26 h-auto mr-5 md:w-26 md:h-auto lg:w-30 lg:h-auto" />
+            <Logo className="w-24 h-auto sm-below:w-16 mr-5 md:w-26 md:h-auto lg:w-30 lg:h-auto" />
           </div>
         </nav>
-        {/* 
-         */}
+        <div className="w-full flex p-24 justify-between">
+          <ul className="list-none pt-20 m-0 flex flex-col flex-grow flex-shrink-0 w-2/3">
+            <hr />
+            <li className="font-lexend-deca text-4xl sm-below:text-xl font-medium leading-[65px] text-left">
+              <div className="flex flex-col">
+                <a href="#">WHO WE ARE ?</a>
+                <a href="#">ስለ እኛ?</a>
+              </div>
+            </li>
+            <hr />
+            <li className="font-lexend-deca text-4xl sm-below:text-xl font-medium leading-[65px] text-left">
+              <div className="flex flex-col">
+                <a href="#">OUR WORKS</a>
+                <a href="#">ስራዎቻችን</a>
+              </div>
+            </li>
+            <hr />
+            <li className="font-lexend-deca text-4xl sm-below:text-xl font-medium leading-[65px] text-left">
+              <div className="flex flex-col">
+                <a href="#">Services</a>
+                <a href="#">አገልግሎታችን</a>
+              </div>
+            </li>
+            <hr />
+            <li className="font-lexend-deca text-4xl sm-below:text-xl font-medium leading-[65px] text-left">
+              <div className="flex flex-col">
+                <a href="#">GET IN TOUCH</a>
+                <a href="#">በዚህ ያግኙን</a>
+              </div>
+            </li>
+            <hr />
+          </ul>
+          <div className="flex items-end justify-end w-1/4">
+            <BackgroundOverlay className=" absolute top-20 right-0 bg-cover bg-center" />
+            <Socials />
+          </div>
+        </div>
       </div>
     </div>
   );
